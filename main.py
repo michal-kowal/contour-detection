0478076589ad914a8a26b33d3043d90efe48ffbc
michal-kowal <kowalewski.michal777@gmail.com>
2023-11-04 18:11:50 +0100

    Mozna powieddzieć że gotowe, ale BEZ centroidów

--- main.py
+++ main.py
@@ -5,11 +5,21 @@ from skimage import measure, io, color, exposure, morphology, filters
 
 def find_contours(image):
     # Zwiększenie kontrastu obrazu
-    contrast_image = exposure.adjust_sigmoid(image, gain=10, cutoff=0.4)
+    contrast_image = exposure.adjust_sigmoid(image, gain=4, cutoff=0.40)
+
+    red_channel = image[:, :, 0]  # Czerwony kanał
+    green_channel = image[:, :, 1]  # Zielony kanał
+    blue_channel = image[:, :, 2]  # Niebieski kanał
+
+    brightness = (red_channel.mean() + green_channel.mean() + blue_channel.mean()) / 3
 
     # Ustal wartości percentyli dla jasności obrazu
-    intensityP = 1
-    intensityK = 25
+    if brightness > 120:
+        intensityP = 1
+        intensityK = 25
+    else:
+        intensityP = 1
+        intensityK = 50
     pp, pk = np.percentile(contrast_image, (intensityP, intensityK))
 
     # Przeskaluj intensywność obrazu
@@ -25,17 +35,16 @@ def find_contours(image):
     threshold_value = filters.threshold_otsu(black_white)
     thresholded_image = black_white > threshold_value
 
-    # thresholded_image = morphology.erosion(thresholded_image, morphology.disk(2))
     for i in range(3):
-        # Dylatacja
-        thresholded_image = morphology.dilation(thresholded_image, morphology.disk(2))
+        thresholded_image = morphology.dilation(thresholded_image, morphology.disk(3))
+
+    # thresholded_image = morphology.erosion(thresholded_image, morphology.disk(1))
 
     # Wygładź obraz filtrem Gaussa
     smoothed_image = filters.gaussian(thresholded_image, sigma=1.5)
 
     # Zastosuj operacje morfologiczne, aby wyostrzyć i zmienić kształt konturów
     smoothed_image = morphology.closing(smoothed_image, morphology.disk(4))
-
     # Znajdź kontury na przetworzonym obrazie
     contours = measure.find_contours(smoothed_image, 0.5)
 
@@ -47,7 +56,7 @@ def find_contours(image):
 
 def main():
     planes = ['planes/samolot00.jpg', 'planes/samolot01.jpg', 'planes/samolot02.jpg', 'planes/samolot03.jpg',
-              'planes/samolot04.jpg', 'planes/samolot05.jpg', 'planes/samolot06.jpg', 'planes/samolot07.jpg',
+              'planes/samolot18.jpg', 'planes/samolot05.jpg', 'planes/samolot19.jpg', 'planes/samolot07.jpg',
               'planes/samolot08.jpg', 'planes/samolot09.jpg', 'planes/samolot10.jpg', 'planes/samolot11.jpg',
               'planes/samolot12.jpg', 'planes/samolot13.jpg', 'planes/samolot14.jpg', 'planes/samolot15.jpg',
               'planes/samolot16.jpg', 'planes/samolot17.jpg']
@@ -64,7 +73,7 @@ def main():
         axes[int(i / 3), i % 3].axis('off')  # Wyłączenie osi na subplotcie
 
         for j, contour in enumerate(contours):
-            axes[int(i / 3), i % 3].plot(contour[:, 1], contour[:, 0], linewidth=2, c=colors[j])
+            axes[int(i / 3), i % 3].plot(contour[:, 1], contour[:, 0], linewidth=4, c=colors[j])
 
     # plt.show()
     plt.savefig('plot.pdf')
